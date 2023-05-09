feceada4c88ea256bcfd5656751b1a6dc5ff1727
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-09 22:58:55 +0200

    Added EF

--- GraphQLProject2/Services/ProductService.cs
+++ GraphQLProject2/Services/ProductService.cs
@@ -1,44 +1,61 @@
-﻿using GraphQLProject.Interfaces;
+﻿using GraphQLProject.Data;
+using GraphQLProject.Interfaces;
 using GraphQLProject.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphQLProject.Services
 {
     public class ProductService : IProduct
     {
-        private static List<Product> products = new List<Product>
-        {
-            new Product { Id = 0, Name = "Coffee", Price = 3.99 },
-            new Product { Id = 1, Name = "Tee", Price = 2.99 },
-            new Product { Id = 2, Name = "Juice", Price = 4.99 },
-            new Product { Id = 3, Name = "Water", Price = 1.99 },
+        //private static List<Product> products = new List<Product>
+        //{
+        //    new Product { Id = 0, Name = "Coffee", Price = 3.99 },
+        //    new Product { Id = 1, Name = "Tee", Price = 2.99 },
+        //    new Product { Id = 2, Name = "Juice", Price = 4.99 },
+        //    new Product { Id = 3, Name = "Water", Price = 1.99 },
 
-        };
+        //};
+        
+        private GraphQLDbContext _dbContext;
+        public ProductService(GraphQLDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
 
         public Product AddProduct(Product product)
         {
-            products.Add(product);
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+
             return product;
         }
 
         public void DeleteProduct(int id)
         {
-            products.RemoveAt(id);
+            var productObj = _dbContext.Products.Find(id);
+            _dbContext.Products.Remove(productObj);
+            _dbContext.SaveChanges();
         }
 
         public List<Product> GetAllProducts()
         {
-            return products;
+            return _dbContext.Products.ToList();
         }
 
         public Product GetProductById(int id)
         {
-            return products.Find(p => p.Id == id);
+            return _dbContext.Products.Find(id);
         }
 
         public Product UpdateProduct(int id, Product product)
         {
-            products[id] = product; 
+            var productObj = _dbContext.Products.Find(id);
+            productObj.Name = product.Name;
+            productObj.Price = product.Price;
+            _dbContext.SaveChanges();
+
             return product;
         }
     }
--- GraphQLProject2/Startup.cs
+++ GraphQLProject2/Startup.cs
@@ -1,6 +1,7 @@
 using GraphiQl;
 using GraphQL.Server;
 using GraphQL.Types;
+using GraphQLProject.Data;
 using GraphQLProject.Interfaces;
 using GraphQLProject.Mutations;
 using GraphQLProject.Query;
@@ -9,6 +10,7 @@ using GraphQLProject.Services;
 using GraphQLProject.Type;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,18 +31,21 @@ namespace GraphQLProject
         {
             services.AddControllers();
             services.AddTransient<IProduct, ProductService>();
-            services.AddSingleton<ProductType>();
-            services.AddSingleton<ProductQuery>();
-            services.AddSingleton<ProductMutation>();
-            services.AddSingleton<ISchema, ProductSchema>();
+            services.AddTransient<ProductType>();
+            services.AddTransient<ProductQuery>();
+            services.AddTransient<ProductMutation>();
+            services.AddTransient<ISchema, ProductSchema>();
             services.AddGraphQL(options =>
             {
                 options.EnableMetrics = false;
             }).AddSystemTextJson();
+
+            //register entity framework
+            services.AddDbContext<GraphQLDbContext>(option => option.UseSqlServer(@"Data Source=DESKTOP-TSGF7QA;Initial Catalog=GraphQL;Integrated Security=True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, GraphQLDbContext graphQLDbContext)
         {
             if (env.IsDevelopment())
             {
@@ -49,7 +54,9 @@ namespace GraphQLProject
 
             app.UseGraphiQl("/graphql");
             app.UseGraphQL<ISchema>();
+            graphQLDbContext.Database.EnsureCreated();
 
+            // This is used for REST API
             //app.UseHttpsRedirection();
             //app.UseRouting();
             //app.UseAuthorization();
