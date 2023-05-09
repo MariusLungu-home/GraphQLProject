62b4886b3686c7311c2634959f792d133a70956e
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-09 18:44:34 +0200

    added mutations

---
+++ GraphQLProject2/Mutations/ProductMutation.cs
@@ -0,0 +1,42 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+using GraphQLProject.Type;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace GraphQLProject.Mutations
+{
+    public class ProductMutation : ObjectGraphType
+    {
+        public ProductMutation(IProduct productService)
+        {
+            Field<ProductType>("createProduct",
+                            arguments: new QueryArguments(new QueryArgument<ProductInputType> { Name = "product" }),
+                            resolve: context =>
+                            {
+                                return productService.AddProduct(context.GetArgument<Product>("product"));
+                            });
+
+            Field<ProductType>("updateProduct",
+                            arguments:  new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" },
+                                        new QueryArgument<ProductInputType> { Name = "product" }),
+                            resolve: context =>
+                            {
+                                var productId = context.GetArgument<int>("id");
+                                var productObj = context.GetArgument<Product>("product");
+
+                                return productService.UpdateProduct(productId, productObj);
+                            });
+
+            Field<StringGraphType>("deleteProduct",
+                            arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+                            resolve: context =>
+                            {
+                                productService.DeleteProduct(context.GetArgument<int>("id"));
+                                return "The product record is deleted successfully.";
+                            });
+
+        }
+    }
+}
--- GraphQLProject2/Startup.cs
+++ GraphQLProject2/Startup.cs
@@ -2,6 +2,7 @@ using GraphiQl;
 using GraphQL.Server;
 using GraphQL.Types;
 using GraphQLProject.Interfaces;
+using GraphQLProject.Mutations;
 using GraphQLProject.Query;
 using GraphQLProject.Schema;
 using GraphQLProject.Services;
@@ -30,6 +31,7 @@ namespace GraphQLProject
             services.AddTransient<IProduct, ProductService>();
             services.AddSingleton<ProductType>();
             services.AddSingleton<ProductQuery>();
+            services.AddSingleton<ProductMutation>();
             services.AddSingleton<ISchema, ProductSchema>();
             services.AddGraphQL(options =>
             {
@@ -49,11 +51,8 @@ namespace GraphQLProject
             app.UseGraphQL<ISchema>();
 
             //app.UseHttpsRedirection();
-
             //app.UseRouting();
-
             //app.UseAuthorization();
-
             //app.UseEndpoints(endpoints =>
             //{
             //    endpoints.MapControllers();
